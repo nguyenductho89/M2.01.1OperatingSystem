07d57e9d1890a643d68fc0b0f8e45c74c2f90525
Nguyễn Đức Thọ <nguyen.duc.tho@framgia.com>
2018-03-25 22:17:57 +0700

    add testing project

---
+++ Testing/Testing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Testing
+//
+//  Created by Nguyễn Đức Thọ on 3/25/18.
+//  Copyright © 2018 Nguyễn Đức Thọ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
